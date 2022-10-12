de8b73062215beeeb5008f36a96dbe2435a2bd4b
yanglin <yanglin@fa>
2022-10-12 19:59:36 +0800

    update header

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -27,9 +27,9 @@ import { Loading } from './Loading'
 import { Page404 } from './Page404'
 import { PageHead } from './PageHead'
 import { PageAside } from './PageAside'
-import { Footer } from './Footer'
+import { Footer } from './Footer_v2'
 import { NotionPageHeader } from './NotionPageHeader'
-import { GitHubShareButton } from './GitHubShareButton'
+// import { GitHubShareButton } from './GitHubShareButton'
 
 import styles from './styles.module.css'
 
@@ -238,8 +238,8 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
   const socialImage = mapImageUrl(
     getPageProperty<string>('Social Image', block, recordMap) ||
-      (block as PageBlock).format?.page_cover ||
-      config.defaultPageCover,
+    (block as PageBlock).format?.page_cover ||
+    config.defaultPageCover,
     block
   )
 
@@ -286,7 +286,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         footer={footer}
       />
 
-      <GitHubShareButton />
+      {/* <GitHubShareButton /> */}
     </>
   )
 }
--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -1,87 +1,104 @@
 import * as React from 'react'
-import cs from 'classnames'
-import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
-import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
-import { Header, Breadcrumbs, Search, useNotionContext } from 'react-notion-x'
 import * as types from 'notion-types'
+import { Box } from '@mui/material'
 
-import { useDarkMode } from 'lib/use-dark-mode'
-import { navigationStyle, navigationLinks, isSearchEnabled } from 'lib/config'
+// import cs from 'classnames'
+// import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+// import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+// import { Header, Breadcrumbs, Search, useNotionContext } from 'react-notion-x'
 
-import styles from './styles.module.css'
+// import { useDarkMode } from 'lib/use-dark-mode'
+// import { navigationStyle, navigationLinks, isSearchEnabled } from 'lib/config'
 
-const ToggleThemeButton = () => {
-  const [hasMounted, setHasMounted] = React.useState(false)
-  const { isDarkMode, toggleDarkMode } = useDarkMode()
+// import styles from './styles.module.css'
 
-  React.useEffect(() => {
-    setHasMounted(true)
-  }, [])
+// const ToggleThemeButton = () => {
+//   const [hasMounted, setHasMounted] = React.useState(false)
+//   const { isDarkMode, toggleDarkMode } = useDarkMode()
 
-  const onToggleTheme = React.useCallback(() => {
-    toggleDarkMode()
-  }, [toggleDarkMode])
+//   React.useEffect(() => {
+//     setHasMounted(true)
+//   }, [])
 
-  return (
-    <div
-      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
-      onClick={onToggleTheme}
-    >
-      {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-    </div>
-  )
-}
+//   const onToggleTheme = React.useCallback(() => {
+//     toggleDarkMode()
+//   }, [toggleDarkMode])
+
+//   return (
+//     <div
+//       className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+//       onClick={onToggleTheme}
+//     >
+//       {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+//     </div>
+//   )
+// }
+
+const NAV_LINK = [
+  {
+    name: "找資源",
+    link: "https://www.daoedu.tw/search",
+    target: "_blank",
+  },
+  {
+    name: "找活動",
+    link: "https://www.daoedu.tw/activities",
+    target: "_blank",
+  },
+  {
+    name: "加入社群",
+    link: "https://www.facebook.com/groups/2237666046370459",
+    target: "_blank",
+  },
+  {
+    name: "部落格",
+    link: null,
+    target: null,
+  },
+];
 
 export const NotionPageHeader: React.FC<{
   block: types.CollectionViewPageBlock | types.PageBlock
-}> = ({ block }) => {
-  const { components, mapPageUrl } = useNotionContext()
-
-  if (navigationStyle === 'default') {
-    return <Header block={block} />
-  }
-
+}> = () => {
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
-
-        <div className='notion-nav-header-rhs breadcrumbs'>
-          {navigationLinks
-            ?.map((link, index) => {
-              if (!link.pageId && !link.url) {
-                return null
-              }
-
-              if (link.pageId) {
-                return (
-                  <components.PageLink
-                    href={mapPageUrl(link.pageId)}
-                    key={index}
-                    className={cs(styles.navLink, 'breadcrumb', 'button')}
-                  >
-                    {link.title}
-                  </components.PageLink>
-                )
-              } else {
-                return (
-                  <components.Link
-                    href={link.url}
-                    key={index}
-                    className={cs(styles.navLink, 'breadcrumb', 'button')}
-                  >
-                    {link.title}
-                  </components.Link>
-                )
-              }
-            })
-            .filter(Boolean)}
-
-          <ToggleThemeButton />
-
-          {isSearchEnabled && <Search block={block} title={null} />}
-        </div>
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", margin: "auto 0" }}>
+          <img src="/logo.png" alt="logo" width={108} />
+          <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            {
+              NAV_LINK.map(({ name, link, target }) => (
+                <Box
+                  key={name}
+                  sx={{
+                    // fontWeight: '500',
+                    margin: "0 24px",
+                    fontSize: '16px',
+                    lineHeight: '140%',
+                    color: "#fff",
+                    cursor: "pointer",
+                  }}
+                >
+                  {link && target
+                    ? (<a href={link} target={target}>{name}</a>)
+                    : (<Box sx={{ borderBottom: "2px solid #fff", paddingBottom: "1px" }}>{name}</Box>)
+                  }
+                </Box>
+              ))
+            }
+          </Box>
+          <Box
+            sx={{
+              fontSize: '16px',
+              lineHeight: '140%',
+              color: "#fff",
+              cursor: "pointer",
+            }}
+          >
+            新增資源
+          </Box>
+        </Box>
       </div>
-    </header>
+    </header >
   )
 }
