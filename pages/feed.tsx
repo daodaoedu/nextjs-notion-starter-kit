fda724beaed84b8ee5b07233703b24809114f10d
yanglin <yanglin@fa>
2022-11-06 00:41:07 +0800

    refactor xml

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -7,110 +7,82 @@ import {
   idToUuid
 } from 'notion-utils'
 import { ExtendedRecordMap } from 'notion-types'
-
+import { NotionPage } from '../components/NotionPage'
 import * as config from 'lib/config'
 import { getSiteMap } from 'lib/get-site-map'
 import { getCanonicalPageUrl } from 'lib/map-page-url'
 import { getSocialImageUrl } from 'lib/get-social-image-url'
 
-// const EXTERNAL_DATA_URL = 'https://blog.daoedu.com';
-// const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
-//     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-//         ${posts
-//     .map(({ id }) => {
-//       return `
-//                 <url>
-//                     <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
-//                 </url>
-//             `;
-//     })
-//     .join('')}
-//     </urlset>
-//     `;
-
-// Reference: https://leerob.io/blog/nextjs-sitemap-robots
-class Feed extends React.Component {
-  static async getInitialProps({ req, res }) {
-    if (req.method !== 'GET') {
-      res.statusCode = 405
-      res.setHeader('Content-Type', 'application/json')
-      res.write(JSON.stringify({ error: 'method not allowed' }))
-      res.end()
-      return { props: {} }
-    }
-
-    const siteMap = await getSiteMap()
-    const ttlMinutes = 24 * 60 // 24 hours
-    const ttlSeconds = ttlMinutes * 60
-
-    const feed = new RSS({
-      title: config.name,
-      site_url: config.host,
-      feed_url: `${config.host}/feed`,
-      language: "zh",
-      ttl: ttlMinutes
-    })
-
-    for (const pagePath of Object.keys(siteMap.canonicalPageMap)) {
-      const pageId = siteMap.canonicalPageMap[pagePath]
-      const recordMap = siteMap.pageMap[pageId] as ExtendedRecordMap
-      if (!recordMap) continue
-
-      const keys = Object.keys(recordMap?.block || {})
-      const block = recordMap?.block?.[keys[0]]?.value
-      if (!block) continue
-
-      const parentPage = getBlockParentPage(block, recordMap)
-      const isBlogPost =
-        block.type === 'page' &&
-        block.parent_table === 'collection' &&
-        parentPage?.id === idToUuid(config.rootNotionPageId)
-      if (!isBlogPost) {
-        continue
-      }
-
-      const title = getBlockTitle(block, recordMap) || config.name
-      const description =
-        getPageProperty<string>('Description', block, recordMap) ||
-        config.description
-      const url = getCanonicalPageUrl(config.site, recordMap)(pageId)
-      const lastUpdatedTime = getPageProperty<number>(
-        'Last Updated',
-        block,
-        recordMap
-      )
-      const publishedTime = getPageProperty<number>('Published', block, recordMap)
-      const date = lastUpdatedTime
-        ? new Date(lastUpdatedTime)
-        : publishedTime
-          ? new Date(publishedTime)
-          : undefined
-      const socialImageUrl = getSocialImageUrl(pageId)
-
-      feed.item({
-        title,
-        url,
-        date,
-        description,
-        enclosure: socialImageUrl
-          ? {
-            url: socialImageUrl,
-            type: 'image/jpeg'
-          }
-          : undefined
-      })
-    }
-
-    const feedText = feed.xml({ indent: true })
-
-    res.setHeader(
-      'Cache-Control',
-      `public, max-age=${ttlSeconds}, stale-while-revalidate=${ttlSeconds}`
-    )
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8')
-    res.write(feedText)
-    res.end()
-  }
+export default function Feed(props) {
+  return <NotionPage {...props} />
 }
 
-export default Feed;
+Feed.getInitialProps = async ({ res }) => {
+  const siteMap = await getSiteMap()
+  const ttlMinutes = 24 * 60 // 24 hours
+  const ttlSeconds = ttlMinutes * 60
+
+  const feed = new RSS({
+    title: config.name,
+    site_url: config.host,
+    feed_url: `${config.host}/feed`,
+    language: "zh",
+    ttl: ttlMinutes
+  })
+
+  for (const pagePath of Object.keys(siteMap.canonicalPageMap)) {
+    const pageId = siteMap.canonicalPageMap[pagePath]
+    const recordMap = siteMap.pageMap[pageId] as ExtendedRecordMap
+    if (!recordMap) continue
+
+    const keys = Object.keys(recordMap?.block || {})
+    const block = recordMap?.block?.[keys[0]]?.value
+    if (!block) continue
+
+    const parentPage = getBlockParentPage(block, recordMap)
+    const isBlogPost =
+      block.type === 'page' &&
+      block.parent_table === 'collection' &&
+      parentPage?.id === idToUuid(config.rootNotionPageId)
+    if (!isBlogPost) {
+      continue
+    }
+
+    const title = getBlockTitle(block, recordMap) || config.name
+    const description =
+      getPageProperty<string>('Description', block, recordMap) ||
+      config.description
+    const url = getCanonicalPageUrl(config.site, recordMap)(pageId)
+    const lastUpdatedTime = getPageProperty<number>(
+      'Last Updated',
+      block,
+      recordMap
+    )
+    const publishedTime = getPageProperty<number>('Published', block, recordMap)
+
+    const socialImageUrl = getSocialImageUrl(pageId)
+
+    feed.item({
+      title,
+      url,
+      date: lastUpdatedTime || publishedTime,
+      description,
+      enclosure: socialImageUrl
+        ? {
+          url: socialImageUrl,
+          type: 'image/jpeg'
+        }
+        : undefined
+    })
+  }
+
+  const feedText = feed.xml({ indent: true })
+
+  res.setHeader(
+    'Cache-Control',
+    `public, max-age=${ttlSeconds}, stale-while-revalidate=${ttlSeconds}`
+  )
+  res.setHeader('Content-Type', 'text/xml; charset=utf-8')
+  res.write(feedText)
+  res.end()
+}
--- pages/sitemap.tsx
+++ pages/sitemap.tsx
@@ -6,79 +6,75 @@ import {
 import { getCanonicalPageUrl } from 'lib/map-page-url'
 import { SitemapStream, streamToPromise } from 'sitemap'
 import { getSiteMap } from 'lib/get-site-map'
-
+import { NotionPage } from '../components/NotionPage'
 import * as config from 'lib/config'
 
-const siteURL = 'https://www.mbpay.com';
+const siteURL = 'https://blog.daoedu.com';
 
-class Sitemap extends React.Component {
-    static async getInitialProps({ res }) {
-        const ttlMinutes = 24 * 60 // 24 hours
-        const ttlSeconds = ttlMinutes * 60
+export default function Sitemap(props) {
+    return <NotionPage {...props} />
+}
 
-        try {
-            const siteMap = await getSiteMap()
-            const smStream = new SitemapStream({ hostname: siteURL });
+Sitemap.getInitialProps = async ({ res }) => {
+    const ttlMinutes = 24 * 60 // 24 hours
+    const ttlSeconds = ttlMinutes * 60
 
-            for (const pagePath of Object.keys(siteMap.canonicalPageMap)) {
-                const pageId = siteMap.canonicalPageMap[pagePath]
-                const recordMap = siteMap.pageMap[pageId]
-                if (!recordMap) continue
+    try {
+        const siteMap = await getSiteMap()
+        const smStream = new SitemapStream({ hostname: siteURL });
 
-                const keys = Object.keys(recordMap?.block || {})
-                const block = recordMap?.block?.[keys[0]]?.value
-                if (!block) continue
+        for (const pagePath of Object.keys(siteMap.canonicalPageMap)) {
+            const pageId = siteMap.canonicalPageMap[pagePath]
+            const recordMap = siteMap.pageMap[pageId]
+            if (!recordMap) continue
 
-                const parentPage = getBlockParentPage(block, recordMap)
-                const isBlogPost =
-                    block.type === 'page' &&
-                    block.parent_table === 'collection' &&
-                    parentPage?.id === idToUuid(config.rootNotionPageId)
-                if (!isBlogPost) {
-                    continue
-                }
+            const keys = Object.keys(recordMap?.block || {})
+            const block = recordMap?.block?.[keys[0]]?.value
+            if (!block) continue
 
-                const url = getCanonicalPageUrl(config.site, recordMap)(pageId)
-                const lastUpdatedTime = new Date(block["last_edited_time"]).toISOString()
-                const publishedTime = new Date(block["created_time"]).toISOString()
-
-                console.log("lastUpdatedTime", lastUpdatedTime);
-                console.log("publishedTime", publishedTime);
-
-
-                smStream.write({
-                    url: url,
-                    changefreq: 'daily',
-                    priority: 1,
-                    lastmod: lastUpdatedTime || publishedTime,
-                });
+            const parentPage = getBlockParentPage(block, recordMap)
+            const isBlogPost =
+                block.type === 'page' &&
+                block.parent_table === 'collection' &&
+                parentPage?.id === idToUuid(config.rootNotionPageId)
+            if (!isBlogPost) {
+                continue
             }
 
-            // End sitemap stream
-            smStream.end();
+            const url = getCanonicalPageUrl(config.site, recordMap)(pageId)
+            const lastUpdatedTime = new Date(block["last_edited_time"]).toISOString()
+            const publishedTime = new Date(block["created_time"]).toISOString()
 
-            // XML sitemap string
-            const sitemapOutput = (await streamToPromise(smStream)).toString();
-
-            res.setHeader(
-                'Cache-Control',
-                `public, max-age=${ttlSeconds}, stale-while-revalidate=${ttlSeconds}`
-            )
-
-            res.setHeader('Content-Type', 'text/xml; charset=utf-8')
-            res.end(sitemapOutput);
-
-        } catch (error) {
-            console.error(error);
-            res.status(500).end();
-            return {
-                props: {},
-            };
+            smStream.write({
+                url: url,
+                changefreq: 'daily',
+                priority: 1,
+                lastmod: lastUpdatedTime || publishedTime,
+            });
         }
+
+        // End sitemap stream
+        smStream.end();
+
+        // XML sitemap string
+        const sitemapOutput = (await streamToPromise(smStream)).toString();
+
+        res.setHeader(
+            'Cache-Control',
+            `public, max-age=${ttlSeconds}, stale-while-revalidate=${ttlSeconds}`
+        )
+
+        res.setHeader('Content-Type', 'text/xml; charset=utf-8')
+        res.end(sitemapOutput);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).end();
         return {
             props: {},
         };
     }
+    return {
+        props: {},
+    };
 }
-
-export default Sitemap;
