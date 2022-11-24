24f09ed7add57a4598a03023294b85515d393fe9
yanglin <yanglin@fa>
2022-11-24 23:19:32 +0800

    migrate from native module to daodao module

--- shared/styles/Global.tsx
+++ shared/styles/Global.tsx
@@ -2,27 +2,7 @@ import React from "react";
 import { Global, css } from "@emotion/react";
 
 const ResetCSS = css`
-  --blue: #007bff;
-  --indigo: #6610f2;
-  --purple: #6f42c1;
-  --pink: #e83e8c;
-  --red: #dc3545;
-  --orange: #fd7e14;
-  --yellow: #ffc107;
-  --green: #28a745;
-  --teal: #20c997;
-  --cyan: #17a2b8;
-  --white: #fff;
-  --gray: #6c757d;
-  --gray-dark: #343a40;
-  --primary: #007bff;
-  --secondary: #6c757d;
-  --success: #28a745;
-  --info: #17a2b8;
-  --warning: #ffc107;
-  --danger: #dc3545;
-  --light: #f8f9fa;
-  --dark: #343a40;
+  
   background-color: #f5f5f5;
   ol,
   ul {
@@ -175,9 +155,9 @@ const ResetCSS = css`
 `;
 
 const GlobalStyle = () => {
-    return (
-        <Global styles={ResetCSS} />
-    );
+  return (
+    <Global styles={ResetCSS} />
+  );
 };
 
 export default GlobalStyle;
